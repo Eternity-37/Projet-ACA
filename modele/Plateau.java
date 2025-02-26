f812804b5a2591b4e2dff495c71b212999415fc3
Eternity <theo.bourgeois@hotmail.com>
2025-02-26 11:25:20 +0100

    tentative ajout plateau

--- modele/Plateau.java
+++ modele/Plateau.java
@@ -1,3 +1,12 @@
-public class Plateau {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
+public class Plateau {
+    ArrayList<ArrayList<String>> plateau = new ArrayList<>();
+    private int n = 8;
+
+    for (int i = 0; i<n ; i++){
+        plateau.add(new ArrayList<>(Collections.nCopies(n, null)));
+    }
 }
