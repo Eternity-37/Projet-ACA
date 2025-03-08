5f71b38ee77d5a92716179d916d70e8b6a7cef82
Eternity <theo.bourgeois@hotmail.com>
2025-03-08 11:18:50 +0100

    vérification du coup finie

--- modele/Jeux.java
+++ modele/Jeux.java
@@ -15,31 +15,36 @@ public class Jeux {
     }
 
     public static boolean coupEstValide(int x, int y, Plateau plateau,int joueurcourant){
-        if (x >=0 && x<8 && y >=0 && y<8) {
-            for (SimpleEntry<Integer,Integer> cases : casesVoisines) {
-                int ligne = cases.getKey();
-                int colonne = cases.getValue();
-                if (x + ligne >= 0 && x + ligne < 8 && y + colonne >= 0 && y + colonne < 8) {
-                    if (Plateau.getCase(x + ligne, y + colonne) != Joueurs.joueurSuivant(joueurcourant)) {
-                        continue;
+        if (x < 0 || x >= 8 || y < 0 || y >= 8) {
+            return false;
+        }
+        if (Plateau.getCase(x, y) != 0 || plateau.getCase(x, y) == Joueurs.joueurSuivant(joueurcourant)) {
+            return false;
+        }
+        for (SimpleEntry<Integer, Integer> direction : casesVoisines) {
+            int ligne = direction.getKey();
+            int colonne = direction.getValue();
+            int lignecourante = x + ligne;
+            int colonnourante = y + colonne;
+            boolean aTrouveAdversaire = false;
+
+            while (lignecourante >= 0 && lignecourante < 8 && colonnourante >= 0 && colonnourante < 8) {
+                int caseCourante = plateau.getCase(lignecourante, colonnourante);
+
+                if (caseCourante == 0) {
+                    break;
+                }
+                if (caseCourante == joueurcourant) {
+                    if (aTrouveAdversaire) {
+                        return true;
+                    }
+                    else {
+                        break;
                     }
                 }
-                x += ligne;
-                y += colonne;
-                while (x>=0 && x<8 && y>=0 && y<8) {
-                    if (x + ligne >= 0 && x + ligne < 8 && y + colonne >= 0 && y + colonne < 8) {
-                        if (Plateau.getCase(x + ligne, y + colonne) == Joueurs.joueurSuivant(joueurcourant)) {
-                            x += ligne;
-                            y += colonne;
-                        }
-                        else if(Plateau.getCase(x+ligne,y+colonne) == joueurcourant){
-                            return true;
-                        }
-
-                    }
-
-                }
-                return false;
+                aTrouveAdversaire = true;
+                lignecourante += ligne;
+                colonnourante += colonne;
             }
         }
         return false;
