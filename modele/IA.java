544e444b3cd048b05954a4837d88d1ccf3b43348
Eternity <theo.bourgeois@hotmail.com>
2025-03-19 14:43:34 +0100

    création algo éval plateau

--- modele/IA.java
+++ modele/IA.java
@@ -6,8 +6,12 @@ import java.util.Random;
 
 public class IA {
 
-    public static AbstractMap.SimpleEntry<Integer,Integer> coupIA(List<AbstractMap.SimpleEntry<Integer, Integer>> coupPossibles){
+    public static AbstractMap.SimpleEntry<Integer,Integer> coupIANaïve(List<AbstractMap.SimpleEntry<Integer, Integer>> coupPossibles){
         Random nbrandom = new Random();
         return coupPossibles.get(nbrandom.nextInt(coupPossibles.size()));
+    }
+
+    public static AbstractMap.SimpleEntry<Integer,Integer> coupIAminmax(List<AbstractMap.SimpleEntry<Integer, Integer>> coupPossibles){
+
     }
 }
